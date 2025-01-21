8bda941cc1830b24c8cadc214e4d467d6116ce1c
Guest User <Guest@TCE-08.local>
2025-01-21 14:13:44 +0500

    camp 999prost6o

--- my_answer.sh
+++ my_answer.sh
@@ -10,3 +10,9 @@ git clone $REPO_URL
 REPO_NAME="the-final-cl-test"
 echo "Содержимое репозитория:"
 ls -la $REPO_NAME
+
+# Создание нового коммита
+cd $REPO_NAME
+echo "# Новый файл" > new_file.txt
+git add new_file.txt
+git commit -m "Добавлен новый файл new_file.txt"
