3df1cc1981671c4fb2154e7f3746e03e1df63f25
Guest User <Guest@TCE-08.local>
2025-01-21 13:48:04 +0500

    camp 999prost4o

--- my_answer.sh
+++ my_answer.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Clone the repository
-git clone https://github.com/01-edu/the-final-cl-test.git the-final-cl-test >/dev/null 2>&1
+# Удаляем предыдущую копию репозитория, если она существует
+rm -rf the-final-cl-test
 
-# Print the contents of the entire repository
-ls -R the-final-cl-test
+# Клонируем репозиторий
+git clone https://github.com/01-edu/the-final-cl-test.git the-final-cl-test
 
+# Проверяем, успешно ли клонирован репозиторий
+if [ -d "the-final-cl-test" ]; then
+  # Если папка существует, выводим содержимое
+  ls -R the-final-cl-test
+else
+  # Если папка не создана, выводим сообщение об ошибке
+  echo "Ошибка: репозиторий не клонирован."
+fi
