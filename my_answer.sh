6643db4e26b52c767113dfeca5c6ede30e4f0b5c
Guest User <Guest@TCE-08.local>
2025-01-21 15:46:59 +0500

    1548

--- my_answer.sh
+++ my_answer.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # Клонирование репозитория
-REPO_URL="https://github.com/01-edu/the-final-cl-test.git"
-TARGET_DIR="the-final-cl-test"
-
-# Проверка, существует ли директория
-if [ ! -d "$TARGET_DIR" ]; then
-    git clone "$REPO_URL"
-fi
-
+git clone https://github.com/01-edu/the-final-cl-test.git
 # Печать строки
 echo "John Doe$"
