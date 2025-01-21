11c4c79b2a4180c98a68abc2946defed3e972f39
Guest User <Guest@TCE-08.local>
2025-01-21 13:17:11 +0500

    camp 99

--- my_answer.sh
+++ my_answer.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# 1. Клонируем репозиторий
+# Clone the repository
 git clone https://github.com/01-edu/the-final-cl-test.git the-final-cl-test >/dev/null 2>&1
 
-# 2. Проверяем, существует ли файл final.txt
-if [ -f "the-final-cl-test/final.txt" ]; then
-  # 3. Извлекаем вторую строку из файла и выводим её
-  head -2 the-final-cl-test/final.txt | tail -1
-else
-  echo "Ошибка: файл final.txt не найден."
-fi
+# Extract the required line and print it
+head -2 the-final-cl-test/final.txt | tail -1
