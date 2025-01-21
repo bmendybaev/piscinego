2b906d59d74f5bdede14ba0642d7b5977fb04964
Guest User <Guest@TCE-08.local>
2025-01-21 14:56:36 +0500

    zadoelo:

--- my_answer.sh
+++ my_answer.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 # URL репозитория
-REPO_URL="https://github.com/01-edu/the-final-cl-test"
+REPO_URL="https://github.com/01-edu/the-final-cl-test.git"
 
 # Клонирование репозитория
 git clone $REPO_URL
 
-# Печать содержимого репозитория
+# Проверка содержимого репозитория
 REPO_NAME="the-final-cl-test"
-echo "Содержимое репозитория:"
+echo "Полное содержимое репозитория:"
 ls -la $REPO_NAME
-
-# Создание нового коммита
-cd $REPO_NAME
-echo "# Новый файл" > new_file.txt
-git add new_file.txt
-git commit -m "Добавлен новый файл new_file.txt"
