92fdc244eb5e813941b2efcfee4e3b56fe1fbd3a
Guest User <Guest@TCE-08.local>
2025-01-21 13:21:05 +0500

    camp 999

--- my_answer.sh
+++ my_answer.sh
@@ -3,5 +3,5 @@
 # Clone the repository
 git clone https://github.com/01-edu/the-final-cl-test.git the-final-cl-test >/dev/null 2>&1
 
-# Extract the required line and print it
-head -2 the-final-cl-test/final.txt | tail -1
+# Print the contents of the file
+cat the-final-cl-test/final.txt
