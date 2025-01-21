21da1d3a8cfed4d2bc3c16211a8ccc14cbd432f6
Guest User <Guest@TCE-08.local>
2025-01-21 13:31:26 +0500

    camp 999prost2o

--- my_answer.sh
+++ my_answer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Clone the repository
-git clone https://github.com/01-edu/the-final-cl-test.git
-ls ]R the-final-cl-test
+git clone https://github.com/01-edu/the-final-cl-test.git the-final-cl-test >/dev/null 2>&1
+
+# Print the contents of the entire repository
+ls -R the-final-cl-test
 
