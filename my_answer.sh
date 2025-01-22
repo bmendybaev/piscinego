637321782ac84ceafe68758371b03c217e18af88
Guest User <Guest@TCE-08.local>
2025-01-22 13:53:04 +0500

    1354@

--- my_answer.sh
+++ my_answer.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Найти путь к директории mystery
-MYSTERY_DIR=$(find . -type d -name "mystery" | head -n 1)
+cd mystery
 
-# Проверить, найден ли путь
-if [ -z "$MYSTERY_DIR" ]; then
-    echo "Directory 'mystery' not found!"
-    exit 1
+# Найти все строки с CLUE и извлечь любые упоминания о murder
+grep "CLUE" crimescene | grep -i "murder" | awk -F':' '{print $2}' | xargs > potential_murderer.txt
+
+if [ -s potential_murderer.txt ]; then
+    MURDERER=$(cat potential_murderer.txt)
+    echo "The murderer is: $MURDERER"
+else
+    echo "DDDDDCould not identify the murderer from the clues!"
 fi
 
-# Перейти в найденную директорию
-cd "$MYSTERY_DIR"
-
-# Найти убийцу
-MURDERER=$(grep "CLUE" crimescene | grep "murderer" | awk -F':' '{print $2}' | xargs)
-echo "The murderer is: $MURDERER"
+# Очистка временного файла
+rm -f potential_murderer.txt
