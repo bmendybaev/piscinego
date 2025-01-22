9f2513ad90433c3a9c0da5d07bfcd3ecae22fefb
Guest User <Guest@TCE-08.local>
2025-01-23 03:21:32 +0500

    2 3-20

--- isnegative.go
+++ isnegative.go
@@ -1,13 +1,8 @@
 
 package piscine
 
+import "fmt"
+
 func IsNegative(nb int) {
-	if nb < 0 {
-		z01.PrintRune('T')
-	} else {
-		z01.PrintRune('F')
-	}
-	z01.PrintRune('\n')
+	fmt.Println(nb)
 }
-
-
