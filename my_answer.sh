0e90be64bef8f50a1533d01960fde56679c89500
Guest User <Guest@TCE-08.local>
2025-01-23 01:15:04 +0500

    I did it?

--- my_answer.sh
+++ my_answer.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-cd mystery
-
-# Найти все строки с CLUE и извлечь любые упоминания о murder
-grep "CLUE" crimescene | grep -i "murder" | awk -F':' '{print $2}' | xargs > potential_murderer.txt
-
-if [ -s potential_murderer.txt ]; then
-    MURDERER=$(cat potential_murderer.txt)
-    echo "The murderer is: $MURDERER"
-else
-    echo "DDDDDCould not identify the murderer from the clues!"
-fi
-
-# Очистка временного файла
-rm -f potential_murderer.txt
+echo "Dartey Henv"
