dc7a7d7301e8bbd707a4e03b460a3914b2c8ad6c
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-01-26 14:34:10 +0000

    Update 'iterativefactorial.go'

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 20 {
+	if nb < 0 || nb > 20 { // Check for non-possible values or overflow
 		return 0
 	}
+
 	result := 1
 	for i := 1; i <= nb; i++ {
 		result *= i
 	}
+
 	return result
 }
