809f3ec5b1aae294eac96dd5dcca72ee11e7c831
Guest User <Guest@TCC-09.local>
2025-01-26 19:49:14 +0500

    sssq

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 20 { // Handle invalid values or overflow
+	if nb < 0 || nb > 20 { // Обрабатываем некорректные значения
 		return 0
 	}
 
 	result := 1
-	for i := 1; i <= nb; i++ {
+	for i := 1; i <= nb; i++ { // Итеративно вычисляем факториал
 		result *= i
 	}
 
