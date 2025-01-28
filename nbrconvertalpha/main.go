d3871857c9b1402408f5d979aa3ec435cb9bb845
Guest User <Guest@TCE-08.local>
2025-01-28 12:38:42 +0500

    W

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"os"
-	"strconv"
 
 	"github.com/01-edu/z01"
 )
 
+// customAtoi преобразует строку в целое число. Если строка некорректна, возвращает -1.
+func customAtoi(s string) int {
+	result := 0
+	for _, r := range s {
+		if r < '0' || r > '9' {
+			return -1 // Если символ не является цифрой, возвращаем -1.
+		}
+		result = result*10 + int(r-'0') // Преобразуем руну в число.
+	}
+	return result
+}
+
 func main() {
-	// Проверяем, передан ли флаг --upper
+	// Проверяем, используется ли флаг --upper
 	upper := false
 	start := 1
 	if len(os.Args) > 1 && os.Args[1] == "--upper" {
@@ -18,10 +29,10 @@ func main() {
 
 	// Проходим по аргументам, начиная с позиции start
 	for i := start; i < len(os.Args); i++ {
-		// Пробуем преобразовать аргумент в число
-		n, err := strconv.Atoi(os.Args[i])
-		if err != nil || n < 1 || n > 26 {
-			// Если ошибка или число вне диапазона, печатаем пробел
+		// Преобразуем аргумент в число с помощью customAtoi
+		n := customAtoi(os.Args[i])
+		if n < 1 || n > 26 {
+			// Если число вне диапазона [1, 26] или аргумент некорректен, печатаем пробел
 			z01.PrintRune(' ')
 		} else {
 			// Преобразуем позицию в букву
