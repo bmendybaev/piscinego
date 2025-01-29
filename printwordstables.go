1078e44adf82e7d4883b79135295e343293309e2
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-01-29 10:09:59 +0000

    Update 'printwordstables.go'

--- printwordstables.go
+++ printwordstables.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-	for i, word := range a {
+	for _, word := range a { // Remove unused `i`
 		for _, char := range word {
 			z01.PrintRune(char) // Print each character
 		}
