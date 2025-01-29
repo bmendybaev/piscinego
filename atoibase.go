483fbc3e129c4cb4e62127229ab3097598b537b8
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-01-29 16:26:37 +0000

    Update 'atoibase.go'

--- atoibase.go
+++ atoibase.go
@@ -1,48 +1,19 @@
-package piscine
+import "github.com/01-edu/z01"
 
-func AtoiBase(s string, base string) int {
-	if !isValidBase(base) {
-		return 0
+func PrintInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
 	}
 
-	baseLen := len(base)
-	result := 0
-
-	for i := 0; i < len(s); i++ {
-		value := indexInBase(s[i], base)
-		if value == -1 {
-			return 0
-		}
-		result = result*baseLen + value
+	var digits []rune
+	for n > 0 {
+		digits = append([]rune{rune(n%10) + '0'}, digits...)
+		n /= 10
 	}
 
-	return result
-}
-
-func isValidBase(base string) bool {
-	if len(base) < 2 {
-		return false
+	for _, digit := range digits {
+		z01.PrintRune(digit)
 	}
-
-	for i := 0; i < len(base); i++ {
-		if base[i] == '+' || base[i] == '-' {
-			return false
-		}
-		for j := i + 1; j < len(base); j++ {
-			if base[i] == base[j] {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func indexInBase(c byte, base string) int {
-	for i := 0; i < len(base); i++ {
-		if base[i] == c {
-			return i
-		}
-	}
-	return -1
+	z01.PrintRune('\n')
 }
