476f9e5a667f2ac9b3a172d244b91de3a7e6db87
bmendyba <bprbirlik@gmail.com>
2025-02-02 16:47:52 +0500

    copilot6

--- point/main.go
+++ point/main.go
@@ -12,42 +12,31 @@ func setPoint(ptr *point) {
 	ptr.y = 21 // Change this value as needed
 }
 
-func printInt(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-
-	digits := [10]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-	div := 1
-	for i := n; i > 9; i /= 10 {
-		div *= 10
-	}
-
-	for div > 0 {
-		digit := n / div
-		z01.PrintRune(digits[digit])
-		n %= div
-		div /= 10
-	}
-}
-
 func main() {
 	points := &point{}
 	setPoint(points)
 
-	z01.PrintRune('x')
-	z01.PrintRune('=')
+	printCharSequence('x', '=')
 	printInt(points.x)
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune('=')
+	printCharSequence(',', ' ', 'y', '=')
 	printInt(points.y)
-	z01.PrintRune('\n')
+	printCharSequence('\n')
+}
+
+func printCharSequence(runes ...rune) {
+	for _, r := range runes {
+		z01.PrintRune(r)
+	}
+}
+
+func printInt(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n >= 10 {
+		printCharSequence('0'+rune(n/10), '0'+rune(n%10))
+	} else {
+		printCharSequence('0' + rune(n))
+	}
 }
