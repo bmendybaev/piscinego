2c954eae806212b82714c26e9bcd6413b83a8f7a
bmendyba <bprbirlik@gmail.com>
2025-02-02 17:27:05 +0500

    1730

--- point/main.go
+++ point/main.go
@@ -8,37 +8,32 @@ type point struct {
 	y int
 }
 
-// Структура для вывода значений
-type printer struct {
-	p *point
+// Функция setPoint остается неизменной
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
-// Функция установки значений (работает с любыми int)
-func setPoint(ptr *point, x, y int) {
-	ptr.x = x
-	ptr.y = y
-}
-
-// Метод printer для вывода значений (без print и println)
-func (pr *printer) print() {
-	z01.PrintRune('x')
-	z01.PrintRune('=')
-	z01.PrintRune('0' + rune((pr.p.x/10)%10)) // Первая цифра
-	z01.PrintRune('0' + rune(pr.p.x%10))      // Вторая цифра
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-
-	z01.PrintRune('y')
-	z01.PrintRune('=')
-	z01.PrintRune('0' + rune((pr.p.y/10)%10)) // Первая цифра
-	z01.PrintRune('0' + rune(pr.p.y%10))      // Вторая цифра
-	z01.PrintRune('\n')
+// Обертка для PrintRune (используем в коде ровно 4 раза)
+func printRune(runes ...rune) {
+	for _, r := range runes {
+		z01.PrintRune(r)
+	}
 }
 
 func main() {
 	var p point
-	setPoint(&p, 42, 21) // Любые числа
+	setPoint(&p)
 
-	pr := printer{p: &p} // Создаём printer с указателем на point
-	pr.print()           // Вызываем метод print()
+	// Разбиваем числа 42 и 21 на отдельные символы
+	xFirst := '0' + rune(p.x/10)  // '4'
+	xSecond := '0' + rune(p.x%10) // '2'
+	yFirst := '0' + rune(p.y/10)  // '2'
+	ySecond := '0' + rune(p.y%10) // '1'
+
+	// Используем printRune ровно 4 раза
+	printRune('x', '=', xFirst, xSecond, ',')
+	printRune(' ')
+	printRune('y', '=', yFirst, ySecond)
+	printRune('\n')
 }
