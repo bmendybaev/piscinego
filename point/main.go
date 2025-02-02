fab36977dbe33dbe58ce5e7fc5179b230b13fcef
bmendyba <bprbirlik@gmail.com>
2025-02-02 15:33:34 +0500

    copilot

--- point/main.go
+++ point/main.go
@@ -1,62 +1,53 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"fmt"
 
-// Структура точки
+	"github.com/01-edu/z01"
+)
+
 type point struct {
-	x, y int
+	x int
+	y int
 }
 
-// Функция установки значений (работает с int)
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
 }
 
+func printNumber(n int) {
+	digits := "0123456789"
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+
+	div := 1
+	for i := n; i > 9; i /= 10 {
+		div *= 10
+	}
+
+	for div > 0 {
+		z01.PrintRune(rune(digits[n/div]))
+		n %= div
+		div /= 10
+	}
+}
+
 func main() {
 	points := &point{}
-	setPoint(points) // Можно менять points.x и points.y
 
-	// Вывод результата
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
+	setPoint(points)
 
-	// Печать десятков
-	if points.x/10 == 4 {
-		z01.PrintRune('4')
-	} else if points.x/10 == 2 {
-		z01.PrintRune('2')
-	}
-
-	// Печать единиц
-	if points.x%10 == 2 {
-		z01.PrintRune('2')
-	} else if points.x%10 == 1 {
-		z01.PrintRune('1')
-	}
-
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-
-	// Печать десятков
-	if points.y/10 == 4 {
-		z01.PrintRune('4')
-	} else if points.y/10 == 2 {
-		z01.PrintRune('2')
-	}
-
-	// Печать единиц
-	if points.y%10 == 2 {
-		z01.PrintRune('2')
-	} else if points.y%10 == 1 {
-		z01.PrintRune('1')
-	}
-
-	z01.PrintRune('\n')
+	fmt.Print("x = ")
+	printNumber(points.x)
+	fmt.Print(", y = ")
+	printNumber(points.y)
+	fmt.Print("\n")
 }
