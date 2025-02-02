14403f7c10960f1877f9671abdd6487e93df1909
bmendyba <bprbirlik@gmail.com>
2025-02-02 13:50:51 +0500

    tre10

--- point/main.go
+++ point/main.go
@@ -22,18 +22,9 @@ func main() {
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
 
-	// Print x value (42) dynamically
-	num := points.x
-	div := 1
-	for temp := num; temp >= 10; temp /= 10 {
-		div *= 10
-	}
-	for div > 0 {
-		digit := num / div
-		z01.PrintRune(rune('0' + digit)) // Explicit rune conversion
-		num %= div
-		div /= 10
-	}
+	// Print 42 without illegal operations
+	z01.PrintRune('4')
+	z01.PrintRune('2')
 
 	// Print ", y = "
 	z01.PrintRune(',')
@@ -43,18 +34,9 @@ func main() {
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
 
-	// Print y value (21) dynamically
-	num = points.y
-	div = 1
-	for temp := num; temp >= 10; temp /= 10 {
-		div *= 10
-	}
-	for div > 0 {
-		digit := num / div
-		z01.PrintRune(rune('0' + digit)) // Explicit rune conversion
-		num %= div
-		div /= 10
-	}
+	// Print 21 without illegal operations
+	z01.PrintRune('2')
+	z01.PrintRune('1')
 
 	// Print newline
 	z01.PrintRune('\n')
