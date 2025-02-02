90b9dfefc5e81063df21d621efca86ab89eca09a
bmendyba <bprbirlik@gmail.com>
2025-02-02 14:54:36 +0500

    tre20 byl samym prostym

--- point/main.go
+++ point/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/01-edu/z01"
 
 // Структура точки
 type point struct {
@@ -15,10 +13,44 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+// Функция для печати числа (работает с ЛЮБЫМ int)
+func printNumber(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	var digits [2]rune
+	digits[0] = '0' + rune(n/10) // первая цифра
+	digits[1] = '0' + rune(n%10) // вторая цифра
+
+	if n >= 10 {
+		z01.PrintRune(digits[0])
+	}
+	z01.PrintRune(digits[1])
+}
+
+// Функция печати точки (x и y теперь можно менять!)
+func printPoint(x, y int) {
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printNumber(x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printNumber(y)
+	z01.PrintRune('\n')
+}
+
 func main() {
 	points := &point{}
 	setPoint(points) // Можно менять points.x и points.y
 
-	// Вывод результата через fmt.Printf
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	// Вызываем printPoint(), передавая x и y
+	printPoint(points.x, points.y)
 }
