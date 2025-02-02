97840003cd4fd21c6f9a41597e72515398efc8fd
bmendyba <bprbirlik@gmail.com>
2025-02-02 12:59:07 +0500

    tre3

--- point/main.go
+++ point/main.go
@@ -1,56 +1,24 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "fmt"
 
+// Define the struct point
 type point struct {
 	x int
 	y int
 }
 
+// Function to set values in the struct
 func setPoint(ptr *point) {
-	ptr.x = '(' * 6 // 42
-	ptr.y = '!' * 3 // 21
-}
-
-func printNbr(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-	var digits []rune
-	sign := false
-	if n < 0 {
-		sign = true
-		n = -n
-	}
-	for n > 0 {
-		digits = append(digits, rune(n%('0'+'1'))+'0')
-		n /= ('0' + '1')
-	}
-	if sign {
-		z01.PrintRune('-')
-	}
-	for i := len(digits) - 1; i >= 0; i-- {
-		z01.PrintRune(digits[i])
-	}
-}
-
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
+	ptr.x = 42
+	ptr.y = 21
 }
 
 func main() {
-	points := &point{}
+	points := &point{} // Create a pointer to a struct
 
-	setPoint(points)
+	setPoint(points) // Modify struct values
 
-	printStr("x = ")
-	printNbr(points.x)
-	printStr(", y = ")
-	printNbr(points.y)
-	z01.PrintRune('\n')
+	// Print values
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
