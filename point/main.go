1959526d40dfd66906ef77786be074bf3790e0f1
bmendyba <bprbirlik@gmail.com>
2025-02-02 12:53:14 +0500

    gpt:2

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -8,8 +10,37 @@ type point struct {
 }
 
 func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+	ptr.x = '(' * 6 // 42
+	ptr.y = '!' * 3 // 21
+}
+
+func printNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var digits []rune
+	sign := false
+	if n < 0 {
+		sign = true
+		n = -n
+	}
+	for n > 0 {
+		digits = append(digits, rune(n%('0'+'1'))+'0')
+		n /= ('0' + '1')
+	}
+	if sign {
+		z01.PrintRune('-')
+	}
+	for i := len(digits) - 1; i >= 0; i-- {
+		z01.PrintRune(digits[i])
+	}
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
 }
 
 func main() {
@@ -17,5 +48,9 @@ func main() {
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	printStr("x = ")
+	printNbr(points.x)
+	printStr(", y = ")
+	printNbr(points.y)
+	z01.PrintRune('\n')
 }
