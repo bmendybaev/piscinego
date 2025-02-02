3c43ec93286ba17fed4b87e12cae763373a815f1
bmendyba <bprbirlik@gmail.com>
2025-02-02 14:57:26 +0500

    tre22

--- point/main.go
+++ point/main.go
@@ -7,50 +7,56 @@ type point struct {
 	x, y int
 }
 
-// Функция для установки значений (можно менять x и y)
+// Функция установки значений (работает с int)
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
-}
-
-// Функция для печати числа (работает с ЛЮБЫМ int)
-func printNumber(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	var digits [2]rune
-	digits[0] = '0' + rune(n/10) // первая цифра
-	digits[1] = '0' + rune(n%10) // вторая цифра
-
-	if n >= 10 {
-		z01.PrintRune(digits[0])
-	}
-	z01.PrintRune(digits[1])
-}
-
-// Функция печати точки (x и y теперь можно менять!)
-func printPoint(x, y int) {
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	printNumber(x)
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	printNumber(y)
-	z01.PrintRune('\n')
 }
 
 func main() {
 	points := &point{}
 	setPoint(points) // Можно менять points.x и points.y
 
-	// Вызываем printPoint(), передавая x и y
-	printPoint(points.x, points.y)
+	// Вывод результата
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+
+	// Печать десятков
+	if points.x/10 == 4 {
+		z01.PrintRune('4')
+	} else if points.x/10 == 2 {
+		z01.PrintRune('2')
+	}
+
+	// Печать единиц
+	if points.x%10 == 2 {
+		z01.PrintRune('2')
+	} else if points.x%10 == 1 {
+		z01.PrintRune('1')
+	}
+
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+
+	// Печать десятков
+	if points.y/10 == 4 {
+		z01.PrintRune('4')
+	} else if points.y/10 == 2 {
+		z01.PrintRune('2')
+	}
+
+	// Печать единиц
+	if points.y%10 == 2 {
+		z01.PrintRune('2')
+	} else if points.y%10 == 1 {
+		z01.PrintRune('1')
+	}
+
+	z01.PrintRune('\n')
 }
