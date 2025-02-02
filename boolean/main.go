b650e55cfd130d410004c0099dd37462f2790129
bmendyba <bprbirlik@gmail.com>
2025-02-02 12:35:02 +0500

    papka

---
+++ boolean/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) boolean {
+	if even(nbr) == 1 {
+		return yes
+	} else {
+		return no
+	}
+}
+
+func main() {
+	if isEven(lengthOfArg) == 1 {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
