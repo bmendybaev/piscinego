a668250ee3a62eccdeea94e053d8fee5771f4600
bmendyba <bprbirlik@gmail.com>
2025-02-02 17:13:41 +0500

    copilot and gpt21

--- point/main.go
+++ point/main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/01-edu/z01"
 
-// Структура для хранения координат
+// Структура для координат
 type point struct {
 	x int
 	y int
@@ -13,31 +13,31 @@ type printer struct {
 	p *point
 }
 
-// Устанавливаем любые значения x и y
+// Функция установки значений (работает с любыми int)
 func setPoint(ptr *point, x, y int) {
 	ptr.x = x
 	ptr.y = y
 }
 
-// Метод printer для вывода значений
+// Метод printer для вывода значений (без print и println)
 func (pr *printer) print() {
 	z01.PrintRune('x')
 	z01.PrintRune('=')
-	z01.PrintRune('0' + rune(pr.p.x/10)) // Первая цифра числа
-	z01.PrintRune('0' + rune(pr.p.x%10)) // Вторая цифра числа
+	z01.PrintRune('0' + rune((pr.p.x/10)%10)) // Первая цифра
+	z01.PrintRune('0' + rune(pr.p.x%10))      // Вторая цифра
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
 
 	z01.PrintRune('y')
 	z01.PrintRune('=')
-	z01.PrintRune('0' + rune(pr.p.y/10)) // Первая цифра числа
-	z01.PrintRune('0' + rune(pr.p.y%10)) // Вторая цифра числа
+	z01.PrintRune('0' + rune((pr.p.y/10)%10)) // Первая цифра
+	z01.PrintRune('0' + rune(pr.p.y%10))      // Вторая цифра
 	z01.PrintRune('\n')
 }
 
 func main() {
 	var p point
-	setPoint(&p, 42, 21) // Передаём любые числа
+	setPoint(&p, 42, 21) // Любые числа
 
 	pr := printer{p: &p} // Создаём printer с указателем на point
 	pr.print()           // Вызываем метод print()
