0dcf772657a6ff23fcc3595114775c0d8068bde0
bmendyba <bprbirlik@gmail.com>
2025-02-02 14:44:09 +0500

    tre19

--- point/main.go
+++ point/main.go
@@ -2,42 +2,53 @@ package main
 
 import "github.com/01-edu/z01"
 
-// Структура для хранения значений
-type Values struct {
-	FortyTwo  string
-	TwentyOne string
-}
-
 // Структура точки
 type point struct {
-	x, y string
+	x, y int
 }
 
-// Функция, изменяющая значения через структуру Values
-func setPoint(ptr *point, v Values) {
-	ptr.x = v.FortyTwo
-	ptr.y = v.TwentyOne
+// Функция для установки значений (оставляем 42 и 21)
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
-func printStr(s string) {
-	for _, char := range s {
-		z01.PrintRune(char)
+// Функция для печати числа
+func printInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var digits [10]rune
+	for i := range digits {
+		digits[i] = '0' + rune(i)
+	}
+	var result []rune
+	for n > 0 {
+		result = append([]rune{digits[n%10]}, result...)
+		n /= 10
+	}
+	for _, d := range result {
+		z01.PrintRune(d)
 	}
 }
 
 func main() {
-	// Создаем структуру со строковыми значениями
-	values := Values{
-		FortyTwo:  "42",
-		TwentyOne: "21",
-	}
-
 	points := &point{}
-	setPoint(points, values)
+	setPoint(points) // Значения останутся 42 и 21
 
-	printStr("x = ")
-	printStr(points.x)
-	printStr(", y = ")
-	printStr(points.y)
+	// Выводим результат
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.y)
 	z01.PrintRune('\n')
 }
