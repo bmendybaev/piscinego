08727e3eeac79708d95d193ecec571b6c673aa71
bmendyba <bprbirlik@gmail.com>
2025-02-02 13:46:04 +0500

    tre9

--- point/main.go
+++ point/main.go
@@ -16,20 +16,46 @@ func main() {
 	points := &point{}
 	setPoint(points)
 
-	// Print "x = 42, y = 21\n" using only z01.PrintRune
+	// Print "x = "
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune('4')
-	z01.PrintRune('2')
+
+	// Print x value (42) dynamically
+	num := points.x
+	div := 1
+	for temp := num; temp >= 10; temp /= 10 {
+		div *= 10
+	}
+	for div > 0 {
+		digit := num / div
+		z01.PrintRune(rune('0' + digit)) // Explicit rune conversion
+		num %= div
+		div /= 10
+	}
+
+	// Print ", y = "
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune('2')
-	z01.PrintRune('1')
+
+	// Print y value (21) dynamically
+	num = points.y
+	div = 1
+	for temp := num; temp >= 10; temp /= 10 {
+		div *= 10
+	}
+	for div > 0 {
+		digit := num / div
+		z01.PrintRune(rune('0' + digit)) // Explicit rune conversion
+		num %= div
+		div /= 10
+	}
+
+	// Print newline
 	z01.PrintRune('\n')
 }
