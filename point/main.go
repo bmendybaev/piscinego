7326dedb5ff58eadb398c40fd500e1c6abc8021a
bmendyba <bprbirlik@gmail.com>
2025-02-02 13:25:50 +0500

    tre6

--- point/main.go
+++ point/main.go
@@ -2,46 +2,58 @@ package main
 
 import "github.com/01-edu/z01"
 
+// Define struct point
 type point struct {
 	x int
 	y int
 }
 
+// Modify function to accept an int pointer
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
+}
+
+// Custom print function using z01.PrintRune
+func printInt(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+
+	var digits []rune
+	for n > 0 {
+		digits = append([]rune{rune(n%10) + '0'}, digits...)
+		n /= 10
+	}
+
+	for _, d := range digits {
+		z01.PrintRune(d)
+	}
 }
 
 func main() {
 	points := &point{}
 	setPoint(points)
 
-	// Print "x = "
+	// Print formatted output without fmt
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-
-	// Print first number (42)
-	if points.x/10 > 0 {
-		z01.PrintRune('0' + rune(points.x/10))
-	}
-	z01.PrintRune('0' + rune(points.x%10))
-
-	// Print ", y = "
+	printInt(points.x)
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
+
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-
-	// Print second number (21)
-	if points.y/10 > 0 {
-		z01.PrintRune('0' + rune(points.y/10))
-	}
-	z01.PrintRune('0' + rune(points.y%10))
-
-	// Print newline
+	printInt(points.y)
 	z01.PrintRune('\n')
 }
