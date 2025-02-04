e9c189f2c347699626db4d341332723e5249e6d1
Guest User <Guest@TCC-09.local>
2025-02-04 16:30:56 +0500

    1632

--- foreach.go
+++ foreach.go
@@ -1,17 +1,20 @@
-package piscine
+package main
 
 import "fmt"
 
 // ForEach применяет переданную функцию f ко всем элементам среза a.
 func ForEach(f func(int), a []int) {
-	// Проходим по каждому элементу среза
 	for _, value := range a {
-		// Вызываем функцию f для текущего элемента
-		f(value)
+		f(value) // Вызываем функцию для каждого элемента
 	}
 }
 
-// PrintNbr выводит число в stdout (имитация PrintNbr из piscine)
+// PrintNbr печатает число без пробела или новой строки.
 func PrintNbr(n int) {
 	fmt.Print(n)
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	ForEach(PrintNbr, a) // Вызываем ForEach с PrintNbr
 }
