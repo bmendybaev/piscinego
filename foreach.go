e7286f04feddd80a4f48c9c6866b598a56a60d91
bmendyba <bprbirlik@gmail.com>
2025-02-05 11:16:55 +0500

    papka1

--- foreach.go
+++ foreach.go
@@ -2,14 +2,14 @@ package piscine
 
 import "fmt"
 
-// ForEach применяет функцию f к каждому элементу среза a
+// ForEach применяет функцию f к каждому элементу среза asdfsdfsdfsd
 func ForEach(f func(int), a []int) {
 	for i := 0; i < len(a); i++ {
 		f(a[i])
 	}
 }
 
-// PrintNbr выводит число без новой строки
+// PrintNbr выводит число без новой строки dsfsdfsdfsdfsdfsdfs
 func PrintNbr(n int) {
 	fmt.Print(n)
 }
