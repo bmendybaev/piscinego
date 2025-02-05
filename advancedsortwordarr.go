08fa1c3b13d87dfa8b56028ac6f22ca2970b9401
bmendyba <bprbirlik@gmail.com>
2025-02-05 14:45:48 +0500

    ssd'

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -1,12 +1,13 @@
 package piscine
 
-import (
-	"sort"
-)
-
 // AdvancedSortWordArr sorts a slice of strings based on the provided comparison function.
 func AdvancedSortWordArr(a []string, f func(a, b string) int) {
-	sort.Slice(a, func(i, j int) bool {
-		return f(a[i], a[j]) < 0
-	})
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if f(a[j], a[j+1]) > 0 { // Swap if out of order
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
 }
