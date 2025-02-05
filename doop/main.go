acaea0cfd78113b3b9b292efc4981c09bcd3e3ec
bmendyba <bprbirlik@gmail.com>
2025-02-05 13:06:08 +0500

    1306

--- doop/main.go
+++ doop/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -11,11 +9,11 @@ func main() {
 		return
 	}
 
-	num1, err1 := strconv.ParseInt(os.Args[1], 10, 64)
+	num1, ok1 := parseInt(os.Args[1])
 	operator := os.Args[2]
-	num2, err2 := strconv.ParseInt(os.Args[3], 10, 64)
+	num2, ok2 := parseInt(os.Args[3])
 
-	if err1 != nil || err2 != nil {
+	if !ok1 || !ok2 {
 		return
 	}
 
@@ -31,13 +29,13 @@ func main() {
 		result, overflow = safeMul(num1, num2)
 	case "/":
 		if num2 == 0 {
-			fmt.Println("No division by 0")
+			os.Stdout.Write([]byte("No division by 0\n"))
 			return
 		}
 		result = num1 / num2
 	case "%":
 		if num2 == 0 {
-			fmt.Println("No modulo by 0")
+			os.Stdout.Write([]byte("No modulo by 0\n"))
 			return
 		}
 		result = num1 % num2
@@ -49,7 +47,51 @@ func main() {
 		return
 	}
 
-	fmt.Println(result)
+	writeInt(result)
+}
+
+func parseInt(s string) (int64, bool) {
+	var num int64
+	var sign int64 = 1
+	for i, c := range s {
+		if i == 0 && c == '-' {
+			sign = -1
+			continue
+		}
+		if c < '0' || c > '9' {
+			return 0, false
+		}
+		num = num*10 + int64(c-'0')
+	}
+	return num * sign, true
+}
+
+func writeInt(n int64) {
+	var buf [20]byte
+	i := len(buf)
+	sign := false
+	if n < 0 {
+		sign = true
+		n = -n
+	}
+
+	for {
+		i--
+		buf[i] = byte('0' + n%10)
+		n /= 10
+		if n == 0 {
+			break
+		}
+	}
+
+	if sign {
+		i--
+		buf[i] = '-'
+	}
+
+	os.Stdout.Write(buf[i:])
+	os.Stdout.Write([]byte{'
+'})
 }
 
 func safeAdd(a, b int64) (int64, bool) {
