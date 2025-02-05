7609dc830f0538f1643f17261d2bf9beebdacda2
bmendyba <bprbirlik@gmail.com>
2025-02-05 11:53:04 +0500

    1153

---
+++ map.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Map(f func(int) bool, a []int) []bool {
+	result := make([]bool, len(a))
+	for i, v := range a {
+		result[i] = f(v)
+	}
+	return result
+}
+
+func IsPrimeCheckcd(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
