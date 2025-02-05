bab1c9313295f27440a43b6eb2094068725ef1e7
bmendyba <bprbirlik@gmail.com>
2025-02-05 12:57:06 +0500

    1258

--- issorted.go
+++ issorted.go
@@ -1,16 +1,22 @@
 package piscine
 
-// IsSorted checks if the slice a is sorted according to the function f
+// IsSorted checks if the slice a is sorted in ascending or descending order according to the function f
 func IsSorted(f func(a, b int) int, a []int) bool {
+	ascending := true
+	descending := true
+
 	for i := 0; i < len(a)-1; i++ {
 		if f(a[i], a[i+1]) > 0 {
-			return false
+			ascending = false
+		}
+		if f(a[i], a[i+1]) < 0 {
+			descending = false
 		}
 	}
-	return true
+	return ascending || descending
 }
 
-// F is a comparison function
-func F(a, b int) int {
-	return a - b
+// IsSortedBy10 compares numbers based on their last digit (modulo 10)
+func IsSortedBy10(a, b int) int {
+	return (a % 10) - (b % 10)
 }
