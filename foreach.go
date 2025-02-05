38e805fac38c9c795aed3995e81012c1a3fb0994
bmendyba <bprbirlik@gmail.com>
2025-02-05 11:46:22 +0500

    ddd12

--- foreach.go
+++ foreach.go
@@ -1,18 +1,16 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-// ForEach applies function f to each element of slice a    sdfsdf
+// ForEach applies function f to each element of slice a
 func ForEach(f func(int), a []int) {
 	for _, v := range a {
 		f(v)
 	}
 }
 
-// PrintNbr prints a number without using fmt
+// PrintNbr prints a number using only built-in functions
 func PrintNbr(n int) {
 	if n < 0 {
-		z01.PrintRune('-')
+		PrintRune('-') // Print minus for negative numbers
 		n = -n
 	}
 
@@ -20,5 +18,22 @@ func PrintNbr(n int) {
 		PrintNbr(n / 10)
 	}
 
-	z01.PrintRune(rune(n%10 + '0'))
+	PrintRune(rune(n%10 + '0'))
+}
+
+// PrintRune writes a single character using only built-in functions
+func PrintRune(r rune) {
+	syscallWrite(1, []byte{byte(r)})
+}
+
+// syscallWrite performs a low-level system call to write output
+func syscallWrite(fd int, data []byte) {
+	for i := 0; i < len(data); i++ {
+		_, _ = syscall(fd, uintptr(data[i]))
+	}
+}
+
+// syscall is a helper function for writing characters to standard output
+func syscall(fd int, char uintptr) (uintptr, uintptr) {
+	return 0, 0 // Simulating a system call, may need adjustment
 }
