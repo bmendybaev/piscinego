f76986d954d9f16a07073f0ad1d9c2fbd26e7621
bmendyba <bprbirlik@gmail.com>
2025-02-05 12:16:08 +0500

    dd1216

--- quest9/main.go
+++ quest9/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-"fmt"
-"piscine"
+	"fmt"
+
+	"piscine"
 )
 
-// sdfsds 1210
+// 1210
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6}
 	result := piscine.Map(piscine.IsPrimeCheckcd, a)
