aaddf5ae0434eb6700085942daea5069d88995a6
bmendyba <bprbirlik@gmail.com>
2025-02-05 12:53:18 +0500

    1253

---
+++ issorted.go
@@ -0,0 +1,16 @@
+package piscine
+
+// IsSorted checks if the slice a is sorted according to the function f
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// F is a comparison function
+func F(a, b int) int {
+	return a - b
+}
