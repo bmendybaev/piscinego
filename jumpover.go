2aafeb9e7360c68b2b7225e8ca6ebfde44a7a769
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 16:48:22 +0000

    ФЫВЫФВФЫ

--- jumpover.go
+++ jumpover.go
@@ -1,34 +1,18 @@
+
 package piscine
-
-import "github.com/01-edu/z01"
-
 func JumpOver(str string) string {
-	// Проверка на пустую строку и ограничения
 	if len(str) < 3 {
-		z01.PrintRune('\n')
 		return "\n"
 	}
-
-	// Разбор строки и сборка заново
-	runes := []rune(str)
-	result := ""
-
-	// Отбор каждого третьего символа
-	for i := 2; i < len(runes); i += 3 {
-		result += string(runes[i])
+	s := ""
+	c := 3
+	for i := 2; i < len(str); i++ {
+		if c == 3 {
+			s += string(str[i])
+			c = 0
+		}
+		c++
 	}
-
-	// Проверка на наличие выбранных символов
-	if result == "" {
-		z01.PrintRune('\n')
-		return "\n"
-	}
-
-	// Вывод результата
-	for _, r := range result {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
-
-	return result + "\n"
+	s += "\n"
+	return s
 }
