69410f5e907223cfe9085058a99d5cc68f9b3596
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 14:58:10 +0000

    Update 'jumpover.go'

--- jumpover.go
+++ jumpover.go
@@ -3,23 +3,15 @@ package piscine
 import "github.com/01-edu/z01"
 
 func JumpOver(str string) string {
-	if str == "1010101010" {
-		z01.PrintRune('\n')
-		return "\n"
-	}
-
 	result := ""
 
 	for i := 2; i < len(str); i += 3 {
 		result += string(str[i])
 	}
 
-	if result == "" {
-		return "\n"
-	}
-
-	for _, r := range result {
-		z01.PrintRune(r)
+	// Отладочный вывод для проверки индексов и символов
+	for i := 2; i < len(str); i += 3 {
+		z01.PrintRune(rune(str[i]))
 	}
 	z01.PrintRune('\n')
 
