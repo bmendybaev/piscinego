1074d65cde6aed2b8d624dfd7a4d2a39c02fbf3e
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 14:28:15 +0000

    сит

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -13,7 +13,25 @@ func capitalize(s string) string {
 	return result
 }
 
+func normalizeString(str string) string {
+	normalized := ""
+	skipSpace := false
+
+	for i := 0; i < len(str); i++ {
+		if str[i] != ' ' {
+			normalized += string(str[i])
+			skipSpace = false
+		} else if !skipSpace {
+			normalized += " "
+			skipSpace = true
+		}
+	}
+
+	return normalized
+}
+
 func ShoppingSummaryCounter(str string) map[string]int {
+	str = normalizeString(str)
 	summary := make(map[string]int)
 	mergedWord := ""
 
