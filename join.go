867e1133313f7d397871f1ab457596d2201a0ead
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 16:08:56 +0000

    ывавыаыв

--- join.go
+++ join.go
@@ -1,14 +1,14 @@
 package piscine
 
 func Join(strs []string, sep string) string {
-    if len(strs) == 0 {
-        return ""
-    }
+	if len(strs) == 0 {
+		return ""
+	}
 
-    result := strs[0] // Start with the first string
-    for i := 1; i < len(strs); i++ {
-        result += sep + strs[i] // Add separator and next string
-    }
+	result := strs[0] // Start with the first string
+	for i := 1; i < len(strs); i++ {
+		result += sep + strs[i] // Add separator and next string
+	}
 
-    return result
+	return result
 }
