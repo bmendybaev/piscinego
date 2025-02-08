f62e2bc240d529b7fba20b6563e406ef7d2e0bb9
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 13:09:52 +0000

    1845

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -7,39 +7,47 @@ import (
 )
 
 func DealAPackOfCards(deck []int) {
-	players := 4
-	cardsPerPlayer := len(deck) / players
+	player1, player2, player3, player4 := deck[0], deck[1], deck[2], deck[3]
+	player5, player6, player7, player8 := deck[4], deck[5], deck[6], deck[7]
+	player9, player10, player11, player12 := deck[8], deck[9], deck[10], deck[11]
 
-	for i := 0; i < players; i++ {
-		fmt.Printf("Player %d: ", i+1)
-		for j := 0; j < cardsPerPlayer; j++ {
-			index := i*cardsPerPlayer + j
-			if j == cardsPerPlayer-1 {
-				printNumber(deck[index])
-			} else {
-				printNumber(deck[index])
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
-		}
-		z01.PrintRune('\n')
-	}
-}
+	fmt.Printf("Player 1: ")
+	z01.PrintRune(rune('0' + player1))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player2))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player3))
+	z01.PrintRune('\n')
 
-func printNumber(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
+	fmt.Printf("Player 2: ")
+	z01.PrintRune(rune('0' + player4))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player5))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player6))
+	z01.PrintRune('\n')
 
-	num := []rune{}
-	for n > 0 {
-		digit := n % 10
-		num = append([]rune{rune('0' + digit)}, num...)
-		n /= 10
-	}
+	fmt.Printf("Player 3: ")
+	z01.PrintRune(rune('0' + player7))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player8))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player9))
+	z01.PrintRune('\n')
 
-	for _, r := range num {
-		z01.PrintRune(r)
-	}
+	fmt.Printf("Player 4: ")
+	z01.PrintRune(rune('0' + player10))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player11))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune('0' + player12))
+	z01.PrintRune('\n')
 }
