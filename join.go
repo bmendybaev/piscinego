343fdc59b5bac24cb66532a000d1faf5cfbe81f7
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 16:12:31 +0000

    фывфы

--- join.go
+++ join.go
@@ -4,11 +4,9 @@ func Join(strs []string, sep string) string {
 	if len(strs) == 0 {
 		return ""
 	}
-
 	result := strs[0] // Start with the first string
 	for i := 1; i < len(strs); i++ {
 		result += sep + strs[i] // Add separator and next string
 	}
-
 	return result
 }
