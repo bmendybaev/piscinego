d62cd76d5b48b8e20d9a911740bb1a181ee8f113
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 06:02:21 +0000

    1100

---
+++ descendcomb.go
@@ -0,0 +1,26 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func DescendComb() {
+	for first := 99; first >= 0; first-- {
+		for second := first - 1; second >= 0; second-- {
+			printNumber(first)
+			z01.PrintRune(' ')
+			printNumber(second)
+
+			if !(first == 1 && second == 0) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func printNumber(n int) {
+	z01.PrintRune(rune('0' + n/10))
+	z01.PrintRune(rune('0' + n%10))
+}
