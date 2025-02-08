8f3194195bfed8127ee846363b1963a6ecec8487
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 15:06:17 +0000

    sd

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,9 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
-    var intSlice []int
-    for _, ch := range str {
-        intSlice = append(intSlice, int(ch))
-    }
-    return intSlice
+	var intSlice []int
+	for _, ch := range str {
+		intSlice = append(intSlice, int(ch))
+	}
+	return intSlice
 }
