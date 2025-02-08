7681a0b3186cb198bd3a969cc0107d1c79bcda82
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 14:51:51 +0000

    фыв

--- jumpover.go
+++ jumpover.go
@@ -3,18 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func JumpOver(str string) string {
-	if len(str) < 3 {
-		z01.PrintRune('\n')
-		return "\n"
-	}
-
 	result := ""
+
 	for i := 2; i < len(str); i += 3 {
 		result += string(str[i])
 	}
 
 	if result == "" {
-		z01.PrintRune('\n')
 		return "\n"
 	}
 
