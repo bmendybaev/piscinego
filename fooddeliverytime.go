3d06bc606a4dfdb762959761acb6c2db955a0187
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 08:05:49 +0000

    1310

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -11,8 +11,8 @@ type food struct {
 func FoodDeliveryTime(order string) int {
 	// Define the menu with corresponding preparation times
 	menu := map[string]food{
-		"burger": {preptime: 15},
-		"chips":  {preptime: 10},
+		"burger":  {preptime: 15},
+		"chips":   {preptime: 10},
 		"nuggets": {preptime: 12},
 	}
 
