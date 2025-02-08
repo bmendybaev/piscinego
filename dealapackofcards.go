f3ffafcbc4efb71e7ff4e939a90e18aff44702d2
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-08 13:11:51 +0000

    Update 'dealapackofcards.go'

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -7,47 +7,81 @@ import (
 )
 
 func DealAPackOfCards(deck []int) {
-	player1, player2, player3, player4 := deck[0], deck[1], deck[2], deck[3]
-	player5, player6, player7, player8 := deck[4], deck[5], deck[6], deck[7]
-	player9, player10, player11, player12 := deck[8], deck[9], deck[10], deck[11]
-
 	fmt.Printf("Player 1: ")
-	z01.PrintRune(rune('0' + player1))
+	printDigits(deck[0])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player2))
+	printDigits(deck[1])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player3))
+	printDigits(deck[2])
 	z01.PrintRune('\n')
 
 	fmt.Printf("Player 2: ")
-	z01.PrintRune(rune('0' + player4))
+	printDigits(deck[3])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player5))
+	printDigits(deck[4])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player6))
+	printDigits(deck[5])
 	z01.PrintRune('\n')
 
 	fmt.Printf("Player 3: ")
-	z01.PrintRune(rune('0' + player7))
+	printDigits(deck[6])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player8))
+	printDigits(deck[7])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player9))
+	printDigits(deck[8])
 	z01.PrintRune('\n')
 
 	fmt.Printf("Player 4: ")
-	z01.PrintRune(rune('0' + player10))
+	printDigits(deck[9])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player11))
+	printDigits(deck[10])
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
-	z01.PrintRune(rune('0' + player12))
+	printDigits(deck[11])
 	z01.PrintRune('\n')
+}
+
+func printDigits(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	digits := []int{}
+	for n > 0 {
+		digits = append([]int{n % 10}, digits...)
+		n /= 10
+	}
+
+	for _, d := range digits {
+		switch d {
+		case 0:
+			z01.PrintRune('0')
+		case 1:
+			z01.PrintRune('1')
+		case 2:
+			z01.PrintRune('2')
+		case 3:
+			z01.PrintRune('3')
+		case 4:
+			z01.PrintRune('4')
+		case 5:
+			z01.PrintRune('5')
+		case 6:
+			z01.PrintRune('6')
+		case 7:
+			z01.PrintRune('7')
+		case 8:
+			z01.PrintRune('8')
+		case 9:
+			z01.PrintRune('9')
+		}
+	}
 }
