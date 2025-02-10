e6539da706be9eb6defd81988e7dfc05a278817d
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 15:05:12 +0000

    йц

--- btreemax.go
+++ btreemax.go
@@ -1,32 +1,5 @@
 package piscine
 
-// TreeNode represents a node in a binary tree
-type TreeNode struct {
-	Data  string
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-// BTreeInsertData inserts data into the binary tree
-func BTreeInsertData(root *TreeNode, data string) {
-	if root == nil {
-		return
-	}
-	if data < root.Data {
-		if root.Left == nil {
-			root.Left = &TreeNode{Data: data}
-		} else {
-			BTreeInsertData(root.Left, data)
-		}
-	} else {
-		if root.Right == nil {
-			root.Right = &TreeNode{Data: data}
-		} else {
-			BTreeInsertData(root.Right, data)
-		}
-	}
-}
-
 // BTreeMax returns the node with the maximum value in the binary tree
 func BTreeMax(root *TreeNode) *TreeNode {
 	if root == nil {
