787f6bab1eb04ade2b4676c91c371174f810b167
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 09:36:43 +0000

    21

---
+++ btreeapplyinorder.go
@@ -0,0 +1,41 @@
+package piscine
+
+// TreeNode defines the structure of a node in the binary search tree
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+// BTreeInsertData inserts new data into the binary search tree following BST properties
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+
+	if data < root.Data {
+		if root.Left == nil {
+			root.Left = &TreeNode{Data: data, Parent: root}
+		} else {
+			BTreeInsertData(root.Left, data)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = &TreeNode{Data: data, Parent: root}
+		} else {
+			BTreeInsertData(root.Right, data)
+		}
+	}
+
+	return root
+}
+
+// BTreeApplyInorder applies a given function f to each element in the tree in inorder traversal
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	BTreeApplyInorder(root.Left, f)
+	f(root.Data)
+	BTreeApplyInorder(root.Right, f)
+}
