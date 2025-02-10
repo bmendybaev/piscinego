aa2e995144a00a22be264438df87983765b54886
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 09:05:48 +0000

    45678

--- listclear.go
+++ listclear.go
@@ -1,13 +1,5 @@
 package piscine
 
-// ListLast returns the Data of the last element in the linked list
-func ListLast(l *List) interface{} {
-	if l.Tail != nil {
-		return l.Tail.Data
-	}
-	return nil
-}
-
 // ListClear deletes all nodes from the linked list
 func ListClear(l *List) {
 	l.Head = nil
