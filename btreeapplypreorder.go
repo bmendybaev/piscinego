3fec05a42d61386bb5e6da810209da34593ea8fa
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 09:58:14 +0000

    6-2

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -1,15 +1,5 @@
 package piscine
 
-// BTreeApplyPostorder applies the function f to each element in the tree using postorder traversal
-func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root == nil {
-		return
-	}
-	BTreeApplyPostorder(root.Left, f)
-	BTreeApplyPostorder(root.Right, f)
-	f(root.Data)
-}
-
 // BTreeApplyPreorder applies the function f to each element in the tree using preorder traversal
 func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
