a2c6dce0eec8e24c91a5c3cc584cb6bf1aa5641e
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 10:12:26 +0000

    8-3

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -1,22 +1,5 @@
 package piscine
 
-// BTreeSearchItem returns the TreeNode with a data field equal to elem if it exists in the tree, otherwise returns nil
-func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root == nil {
-		return nil
-	}
-
-	if root.Data == elem {
-		return root
-	}
-
-	if left := BTreeSearchItem(root.Left, elem); left != nil {
-		return left
-	}
-
-	return BTreeSearchItem(root.Right, elem)
-}
-
 // BTreeLevelCount returns the number of levels of the binary tree (height of the tree)
 func BTreeLevelCount(root *TreeNode) int {
 	if root == nil {
