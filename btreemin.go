d604b6bc7b5e84bd404b84f80164c267e7588510
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 15:12:27 +0000

    16-1

--- btreemin.go
+++ btreemin.go
@@ -11,15 +11,3 @@ func BTreeMax(root *TreeNode) *TreeNode {
 	}
 	return current
 }
-
-// BTreeMin returns the node with the minimum value in the binary tree
-func BTreeMin(root *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
-	}
-	current := root
-	for current.Left != nil {
-		current = current.Left
-	}
-	return current
-}
