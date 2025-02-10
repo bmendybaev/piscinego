86f24ec90cf3590ae4f314c5af6242297794ac01
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 08:59:17 +0000

    56789

--- listlast.go
+++ listlast.go
@@ -1,16 +1,5 @@
 package piscine
 
-// ListSize returns the number of elements in the linked list
-func ListSize(l *List) int {
-	count := 0
-	current := l.Head
-	for current != nil {
-		count++
-		current = current.Next
-	}
-	return count
-}
-
 // ListLast returns the Data of the last element in the linked list
 func ListLast(l *List) interface{} {
 	if l.Tail != nil {
