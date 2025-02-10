4e24f3c6d6dae5fa9e907222e88ccd3b8ec4dc3f
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 11:59:18 +0000

    145

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,32 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Data   string
-	Left   *TreeNode
-	Right  *TreeNode
-	Parent *TreeNode
-}
-
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
-	}
-	if data < root.Data {
-		if root.Left == nil {
-			root.Left = &TreeNode{Data: data, Parent: root}
-		} else {
-			BTreeInsertData(root.Left, data)
-		}
-	} else {
-		if root.Right == nil {
-			root.Right = &TreeNode{Data: data, Parent: root}
-		} else {
-			BTreeInsertData(root.Right, data)
-		}
-	}
-	return root
-}
-
 func BTreeIsBinary(root *TreeNode) bool {
 	return isBST(root, "", "")
 }
@@ -43,31 +16,4 @@ func isBST(node *TreeNode, min, max string) bool {
 	}
 
 	return isBST(node.Left, min, nodeData) && isBST(node.Right, nodeData, max)
-}
-
-func BTreeApplyByLevel(root *TreeNode, f func(string, int)) {
-	if root == nil {
-		return
-	}
-
-	type NodeLevel struct {
-		node  *TreeNode
-		level int
-	}
-
-	queue := []NodeLevel{{node: root, level: 0}}
-
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-
-		f(current.node.Data, current.level)
-
-		if current.node.Left != nil {
-			queue = append(queue, NodeLevel{node: current.node.Left, level: current.level + 1})
-		}
-		if current.node.Right != nil {
-			queue = append(queue, NodeLevel{node: current.node.Right, level: current.level + 1})
-		}
-	}
 }
