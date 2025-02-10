cdcae36ce60b79f77c3dbcedbaf25cb7a6afb847
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 10:39:58 +0000

    11-1

---
+++ btreeapplybylevel.go
@@ -0,0 +1,20 @@
+package piscine
+
+// BTreeIsBinary returns true only if the tree given by root follows the binary search tree properties
+func BTreeIsBinary(root *TreeNode) bool {
+	return isBST(root, "", "")
+}
+
+func isBST(node *TreeNode, min, max string) bool {
+	if node == nil {
+		return true
+	}
+
+	nodeData := node.Data.(string) // Ensure node.Data is a string
+
+	if (min != "" && nodeData <= min) || (max != "" && nodeData >= max) {
+		return false
+	}
+
+	return isBST(node.Left, min, nodeData) && isBST(node.Right, nodeData, max)
+}
