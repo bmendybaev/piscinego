ddc72655f8a9944da6d22ea2975b48f146b9788b
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 12:01:19 +0000

    й

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,19 +1,3 @@
 package piscine
 
-func BTreeIsBinary(root *TreeNode) bool {
-	return isBST(root, "", "")
-}
-
-func isBST(node *TreeNode, min, max string) bool {
-	if node == nil {
-		return true
-	}
-
-	nodeData := node.Data
-
-	if (min != "" && nodeData <= min) || (max != "" && nodeData >= max) {
-		return false
-	}
-
-	return isBST(node.Left, min, nodeData) && isBST(node.Right, nodeData, max)
-}
+// Using existing BTreeIsBinary and isBST defined elsewhere.
