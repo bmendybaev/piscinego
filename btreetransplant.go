a89d2ab317a84f4f8dee6eaae9ce1f2693f3902b
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 15:25:27 +0000

    w

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,27 +1,5 @@
 package piscine
 
-// BTreeMin returns the node with the minimum value in the binary tree
-func BTreeMin(root *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
-	}
-
-	minNode := root
-
-	leftMin := BTreeMin(root.Left)
-	rightMin := BTreeMin(root.Right)
-
-	if leftMin != nil && leftMin.Data < minNode.Data {
-		minNode = leftMin
-	}
-
-	if rightMin != nil && rightMin.Data < minNode.Data {
-		minNode = rightMin
-	}
-
-	return minNode
-}
-
 // BTreeTransplant replaces the subtree rooted at node with the subtree rooted at rplc
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	if root == nil || node == nil {
