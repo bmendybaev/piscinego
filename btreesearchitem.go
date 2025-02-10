4efac7d3792f87df078f60b2bb009bad65c148c7
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 10:03:02 +0000

    7-2

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,15 +1,5 @@
 package piscine
 
-// BTreeApplyPreorder applies the function f to each element in the tree using preorder traversal
-func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root == nil {
-		return
-	}
-	f(root.Data)
-	BTreeApplyPreorder(root.Left, f)
-	BTreeApplyPreorder(root.Right, f)
-}
-
 // BTreeSearchItem returns the TreeNode with a data field equal to elem if it exists in the tree, otherwise returns nil
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
