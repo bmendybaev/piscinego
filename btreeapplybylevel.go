712560dec2c6bab32b9e18abece95a37deba2b1e
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 12:14:18 +0000

    121212

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -23,24 +23,22 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 		return
 	}
 
-	type NodeLevel struct {
-		node  *TreeNode
-		level int
-	}
-
-	queue := []NodeLevel{{node: root, level: 0}}
+	queue := []*TreeNode{root}
 
 	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
 
-		f(current.node.Data, current.level)
+			f(node.Data)
 
-		if current.node.Left != nil {
-			queue = append(queue, NodeLevel{node: current.node.Left, level: current.level + 1})
-		}
-		if current.node.Right != nil {
-			queue = append(queue, NodeLevel{node: current.node.Right, level: current.level + 1})
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
 		}
 	}
 }
