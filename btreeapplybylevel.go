6817a24aab313f8bc4f09b89c8b77ec006384dc5
Birlik Mendybayev <bmendyba@noreply.01.tomorrow-school.ai>
2025-02-10 11:08:43 +0000

    11

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -49,72 +49,11 @@ func isBST(node *TreeNode, min, max string) bool {
 	return isBST(node.Left, min, nodeData) && isBST(node.Right, nodeData, max)
 }
 
-func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root == nil {
-		return nil
-	}
-
-	if root.Data == elem {
-		return root
-	}
-
-	if left := BTreeSearchItem(root.Left, elem); left != nil {
-		return left
-	}
-
-	return BTreeSearchItem(root.Right, elem)
-}
-
-func BTreeLevelCount(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-
-	leftHeight := BTreeLevelCount(root.Left)
-	rightHeight := BTreeLevelCount(root.Right)
-
-	if leftHeight > rightHeight {
-		return leftHeight + 1
-	}
-	return rightHeight + 1
-}
-
 func main() {
 	root := &TreeNode{Data: "4"}
 	BTreeInsertData(root, "1")
 	BTreeInsertData(root, "7")
 	BTreeInsertData(root, "5")
 
-	fmt.Println("Is Binary Search Tree:", BTreeIsBinary(root))
-
-	selected := BTreeSearchItem(root, "7")
-	fmt.Print("Item selected -> ")
-	if selected != nil {
-		fmt.Println(selected.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Parent of selected item -> ")
-	if selected != nil && selected.Parent != nil {
-		fmt.Println(selected.Parent.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Left child of selected item -> ")
-	if selected != nil && selected.Left != nil {
-		fmt.Println(selected.Left.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Print("Right child of selected item -> ")
-	if selected != nil && selected.Right != nil {
-		fmt.Println(selected.Right.Data)
-	} else {
-		fmt.Println("nil")
-	}
-
-	fmt.Println("Tree Level Count:", BTreeLevelCount(root))
+	fmt.Println(BTreeIsBinary(root))
 }
